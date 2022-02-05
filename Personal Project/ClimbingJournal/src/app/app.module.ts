ba1883fec75b8d5d16566460e739d56cb6f8fde6
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2022-02-05 19:14:46 +0100

    BaseLogin
    Introducing basic authentication module (blank) and removing placeholder HTML

--- Personal Project/ClimbingJournal/src/app/app.module.ts
+++ Personal Project/ClimbingJournal/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AuthenticationModule} from "./authentication/authentication.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
